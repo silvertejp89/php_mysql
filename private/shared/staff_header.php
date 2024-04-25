d2069bcbde9f175136014be6d633e25aa74bef99
Kriss Stockhaus <kriss.stockhaus@gmail.com>
2024-04-25 11:34:43 +0200

    fix - links and URLs (css)

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -11,8 +11,7 @@ if (!isset($page_title)) {
 <head>
     <title>GBI - <?php echo $page_title; ?></title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="../css/staff.css">
-    <!-- Obs! man måste skriva in index.php i webbläsaren för att css ska fungera: http://localhost:8000/public/staff/index.php  -->
+    <link rel="stylesheet" href="/../css/staff.css">
 </head>
 
 <body>
@@ -20,8 +19,8 @@ if (!isset($page_title)) {
         <h1>GBI Staff Area</h1>
     </header>
 
-    <navigation>
+    <nav>
         <ul>
-            <li> <a href="index.php">Menu</a> </li>
+            <li> <a href="/staff/index.php">Menu</a> </li>
         </ul>
-    </navigation>
+    </nav>
